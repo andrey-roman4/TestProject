7a072c64921e1b0d48d95a7a95652bd75786cafc
Andrey Romanenko <aromanenko@artec3d.com>
2021-10-16 00:14:20 +0200

    Add Login page locators, login page file and tests in login page

--- test_main_page.py
+++ test_main_page.py
@@ -1,12 +1,28 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                    # открываем страницу
-    page.go_to_login_page          # выполняем метод страницы — переходим на страницу логина
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+def test_login_is_in_login_url(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_url()
+def test_quest_can_see_login_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_login_form()
+def test_quest_can_see_register_form(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+    page = LoginPage(browser, link)
+    page.open()
+    page.should_be_register_form()
